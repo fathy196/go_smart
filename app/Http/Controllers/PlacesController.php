e7f8fd94a32547ab2158fee85092a3f83ed5ea64
ziadadel001 <ziadadel00120@gmail.com>
2024-04-17 00:46:40 +0200

    zz

--- app/Http/Controllers/PlacesController.php
+++ app/Http/Controllers/PlacesController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class PlacesController extends Controller
 {
-    //
+    public function CitiesAction()
+    {
+        $citys = City::all();
+
+        return $citys;
+    }
+    public function ShowOneCity($cityid)
+    {
+        $city = City::find($cityid);
+        if (!$city) {
+            return " 404";
+        }
+        return $city;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlacesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 
 Route::post('login', [AuthController::class, 'login']);
+
+Route::get('/Cities/Explore', [PlacesController::class, 'CitiesAction']);
+Route::get('/Cities/Explore/{Cityid}', [PlacesController::class, 'ShowOneCity']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
